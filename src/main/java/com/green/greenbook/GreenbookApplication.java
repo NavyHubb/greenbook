bd3f5b0a2530a6d66f2084f8736f2fbb471ca7b4
NavyHubb <sngjae972@gmail.com>
2023-05-05 16:41:14 +0900

    Initial Commit

---
+++ src/main/java/com/green/greenbook/GreenbookApplication.java
@@ -0,0 +1,13 @@
+package com.green.greenbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenbookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GreenbookApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/green/greenbook/GreenbookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.green.greenbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenbookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
